dfa43397ff7695d24d2cf191e5929cf402995c62
govindgemexi <govindtotla@gmail.com>
2020-03-14 18:28:22 +0530

    src

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Dashboard from '@views/SignUp';
+import { Main as Layout } from '@layouts';
+
+const Login = () => (
+  <Layout>
+    <Head>
+      <title>Login</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <SignUp />
+  </Layout>
+);
+
+export default Login;

a967ea164e68681817c99ed48e121a9107df8c46
govindgemexi <govindtotla@gmail.com>
2020-04-15 14:54:38 +0530

    stone-host

--- pages/stones/index.js
+++ pages/stones/index.js
@@ -14,39 +14,14 @@ const Stones = props => (
   </Layout>
 );
 
-/*
+
 Stones.getInitialProps = async function() {
-  const res = await fetch('http://localhost:8080/api/stones');
+  const res = await fetch('/api/stones');
   const data = await res.json();
   console.log(`Show data fetched. Count: ${data.length}`);
   return {
     stones: data
   };
 };
-*/
-
-
-
-Stones.getInitialProps = async ctx => {
-  if (ctx.req) {
-    const host = ctx.req.headers['x-forwarded-host'];
-    const proto = ctx.req.headers['x-forwarded-proto'];
-    const port =  ctx.req.headers['x-forwarded-port'];
-
-    const res = await fetch(`${proto}//${host}:${port}/api/stones`);
-    const json = await res.json();
-    
-    return { stones: json };
-  } else {
-    // otherwise we are in the browser
-    const res = await fetch(`/api/posts`);
-
-    const json = await res.json();
-    return { stones: json };
-  }
-}
-
-
-
 
 export default Stones;
